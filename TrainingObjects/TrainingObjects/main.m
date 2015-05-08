10e7438e8caf4a0c75d2efc2912fb0d6bfb8ab53
Julio <julio.castillo@mercadolibre.com>
2015-05-08 12:26:41 -0300

    Adding new porject: working with objects

---
+++ TrainingObjects/TrainingObjects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TrainingObjects
+//
+//  Created by Julio Castillo on 5/8/15.
+//  Copyright (c) 2015 Julio Castillo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
